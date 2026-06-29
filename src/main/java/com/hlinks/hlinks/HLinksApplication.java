e175a2994d543e8a693d48bb4deb36dcaee3f53c
woodgeon <ad4086@naver.com>
2026-06-29 14:57:56 +0900

    Initial commit

---
+++ src/main/java/com/hlinks/hlinks/HLinksApplication.java
@@ -0,0 +1,13 @@
+package com.hlinks.hlinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HLinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HLinksApplication.class, args);
+    }
+
+}
