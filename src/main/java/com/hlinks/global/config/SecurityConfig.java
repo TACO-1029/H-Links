9730a906f58f26cdc052e7807c572b090e875899
woodgeon <ad4086@naver.com>
2026-06-30 17:25:55 +0900

    feat: 성공 및 실패 에러 메세지 표시

--- src/main/java/com/hlinks/global/config/SecurityConfig.java
+++ src/main/java/com/hlinks/global/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/login?logout")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 )
