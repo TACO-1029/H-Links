815a2f5faaaa0e7cae54a05159e31afd1c3c593a
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-06-30 17:30:37 +0900

    feat: 세션 기반 Spring Security 적용 (#9)
    * feat: 세션 기반의 Spring Security 적용
    
    * feat: Security Filter 생성
    
    * feat: CustomUserDetails 구현
    
    * feat: 기능 동작 확인을 위한 임시 페이지 추가
    
    * feat: 성공 및 실패 에러 메세지 표시

---
+++ src/main/java/com/hlinks/global/security/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package com.hlinks.global.security;
+
+import com.hlinks.domain.user.dto.LoginUserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Spring Security는 우리가 만든 LoginUserDto를 바로 인증 객체로 쓰지 않고, UserDetails 타입을 요구합니다.
+ * 이에따라 LoginUserDto + roleCodes를 Spring Security가 이해하는 형태로 포장
+ */
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final LoginUserDto user;
+    private final List<String> roleList;
+
+    // getUserId, getName 다 로그인 이후에 컨트롤러나 서비스에서 현재 로그인 사용자의 정보를 꺼낼 때 쓰는 편의 메서드
+    public Long getUserId() {
+        return user.getUserId();
+    }
+
+    public String getName() {
+        return user.getName();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roleList.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return "ACTIVE".equals(user.getStatus());
+    }
+}
