2432f77d83ea916a020808a6ef3ac6176089c1be
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-07-01 16:28:27 +0900

    feat: 헤더 사용자 프로필 정보 표시

--- src/main/java/com/hlinks/domain/user/dto/LoginUserDto.java
+++ src/main/java/com/hlinks/domain/user/dto/LoginUserDto.java
@@ -7,6 +7,16 @@ import lombok.Setter;
 @Setter
 public class LoginUserDto {
     private Long userId;
+
+    private Long departmentId;
+    private String departmentName;
+
+    private Long jobId;
+    private String jobName;
+
+    private Long positionId;
+    private String positionName;
+
     private String loginId;
     private String password;
     private String name;
