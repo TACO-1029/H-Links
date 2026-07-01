8729471cc65dff2ae5e891550194fabafee83ad3
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-07-01 12:18:42 +0900

    feat: 헤더 활성 메뉴 조건 처리

--- src/main/java/com/hlinks/domain/hr/controller/HrController.java
+++ src/main/java/com/hlinks/domain/hr/controller/HrController.java
@@ -1,12 +1,14 @@
 package com.hlinks.domain.hr.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HrController {
   @GetMapping("/hr")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("activeMenu", "hr");
     return "hr/index";
   }
 }
--- src/main/java/com/hlinks/global/web/MainController.java
+++ src/main/java/com/hlinks/global/web/MainController.java
@@ -1,13 +1,15 @@
 package com.hlinks.global.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping({"/", "/home"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("activeMenu", "home");
 
         return "home/index";
     }
