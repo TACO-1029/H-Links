a7c90270b5dedd46abe4e4423e9052b5e115e2e6
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-07-02 14:37:41 +0900

    fix: STT 응답 DTO unknown field 무시 처리

--- src/main/java/com/hlinks/domain/quiz/stt/dto/SttTranscriptionResponse.java
+++ src/main/java/com/hlinks/domain/quiz/stt/dto/SttTranscriptionResponse.java
@@ -1,10 +1,12 @@
 package com.hlinks.domain.quiz.stt.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SttTranscriptionResponse {
     private String text;
 }
