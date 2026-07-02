0019489dbd19661e482d1a712d1c238f0060a7fd
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-07-02 17:34:31 +0900

    feat: ChapterResponse DTO

---
+++ src/main/java/com/hlinks/domain/course/dto/ChapterResponseDto.java
@@ -0,0 +1,21 @@
+package com.hlinks.domain.course.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChapterResponseDto {
+    private Long chapterId;
+    private Long courseId;
+    private String chapterTitle;
+    private Integer chapterOrder;
+    private String videoUrl;
+    private Integer durationSeconds;
+    private String summaryText;
+    private String transcriptText;
+}
