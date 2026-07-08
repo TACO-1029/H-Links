80cd8563ce4c1a1293f52f2ec11751fd8f501711
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-07-08 09:23:00 +0900

    chore: S3 클라이언트 타임아웃 설정 추가

--- src/main/java/com/hlinks/global/config/S3Config.java
+++ src/main/java/com/hlinks/global/config/S3Config.java
@@ -6,9 +6,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.time.Duration;
 
 @Configuration
 @EnableConfigurationProperties(S3StorageProperties.class)
@@ -20,6 +23,10 @@ public class S3Config {
         return S3Client.builder()
                 .region(Region.of(properties.getRegion()))
                 .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .overrideConfiguration(ClientOverrideConfiguration.builder()
+                        .apiCallTimeout(Duration.ofSeconds(30))
+                        .apiCallAttemptTimeout(Duration.ofSeconds(10))
+                        .build())
                 .build();
     }
 
